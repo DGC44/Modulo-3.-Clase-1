907123ac71cc5d008f7dac33ec9fbe1759433696
DianaGC <garcapdia@hotmail.com>
2025-02-05 17:55:32 -0600

    tipos de datos

---
+++ Untitled-1.js
@@ -0,0 +1,9 @@
+//Comentario de una linea
+/* Comentarios largos */
+
+a = 1
+b = 2
+
+console.log(a + b)
+console.log(typeof 42);
+// number
---
+++ Untitled-2.js
@@ -0,0 +1,10 @@
+// Una funcion que obtiene dos valores y calcula el promedio
+function promedio(a,b) {
+    /*
+     * Calcula el promedio de dos valores.
+     */
+    return (a+b) / 2;
+}
+
+console.log(promedio(10,20));
+
