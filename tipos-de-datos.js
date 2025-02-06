10a4543181f83eb405ef60ad89f0309801c45bc7
DianaGC <garcapdia@hotmail.com>
2025-02-05 18:02:50 -0600

    ejercicio tipos de datos

---
+++ tipos-de-datos.js
@@ -0,0 +1,24 @@
+console.log(typeof 42);
+//number
+console.log(typeof 'twenty20');
+//string
+console.log(typeof '123');
+//string
+console.log(typeof -68);
+//number
+console.log(typeof true);
+//boolean
+console.log(typeof FALSE);
+//undefined (boolean must be lower case)
+console.log(typeof null);
+//null
+console.log(typeof 'true');
+//string
+console.log(typeof 0);
+//number
+console.log(typeof undefined);
+//undefined
+console.log(typeof 55);
+//number
+console.log(typeof -666);
+//number
