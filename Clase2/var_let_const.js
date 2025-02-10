b86e32dbf9abc376beb7c04ff3ad8e68cd7b3252
DianaGC <garcapdia@hotmail.com>
2025-02-10 16:43:13 -0600

    primera parte

--- Clase2/var_let_const.js
+++ Clase2/var_let_const.js
@@ -1,4 +1,19 @@
 var a = "palabra"
 var b = 42
 
-console.log(b)
+var a = "otra cosa"
+
+// ------------------------------
+
+let l ="esta"
+// Solo se puede declarar una vez
+// let l ="esta otra" , no funcionara
+
+
+// ------------------------------
+
+const c= "constante"
+// const c = "variable" Esta no funcionara
+// c = "variable" Esta tampoco
+
+console.log(c) // Saber que hay en la cajita
