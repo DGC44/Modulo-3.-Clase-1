3fedc0146654cc493d548254d1fe7fce9e47299b
DianaGC <garcapdia@hotmail.com>
2025-02-10 17:09:31 -0600

    operadores comparacion

--- Clase2/operadores_comparacion.js
+++ Clase2/operadores_comparacion.js
@@ -1,10 +1,45 @@
+// Igualdad debil
+let a = "palabra"
+let b = "palabra"
+
+console.log(a == b) // True
+
+let x = 0
+let y = "0"
+
+console.log(x == y) // True
+
 // Igualdad fuerte
 console.log(a === b) // True
-//Mayor que 
+console.log(x === y) // False
+
+// Desigualdad debil
+console.log(a != b) // False
+console.log(x != y) // False
+
+// Desigualdad fuerte
+console.log(a !== b) // False
+console.log(x !== y) // True
+
+// Mayor que
 let z = 10
 let w = 5
 
-console.log(z > w) //True
+console.log(z > w) // True
 
-//Menor o igual que 
+// Menor que
+console.log(z < w) // False
+
+// Mayor o igual que
+console.log(z >= w) // True
+
+// Menor o igual que
 console.log(z <= w) // False
+
+
+// Ejemplo practico
+let edad_minima = 18
+
+let edad = 17
+
+console.log(edad >= edad_minima)
