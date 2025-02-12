fe78c746200773282872ae380607e78e837d096d
DianaGC <garcapdia@hotmail.com>
2025-02-11 19:08:00 -0600

    clase3

---
+++ Clase3/sin_ciclos.js
@@ -0,0 +1,68 @@
+/* ciclo {
+... dentro que se repite
+}*/
+
+let a = 5
+
+// Multiplicacion (no existe el *)
+
+// res = 5 * 10
+
+a = a + 5
+a = a + 5
+a = a + 5
+a = a + 5
+a = a + 5
+a = a + 5
+a = a + 5
+a = a + 5
+a = a + 5
+a = a + 5
+
+console.log(a)
+
+/* Mientras los ciclos /iteraciones sean menores a 10  {
+    a = a + 5
+}*/
+
+let i = 10 // Numero de iteraciones
+let b = 0
+
+while (i < 10) {
+    b = b + 5
+    i = i + 1
+}
+
+console.log(b)
+
+// Equivalente a
+
+i = 0
+b = 0
+
+if (i < 10) {
+    b = b + 5
+    i = i + 1
+}
+
+if (i < 10) {
+    b = b + 5
+    i = i + 1
+}
+
+if (i < 10) {
+    b = b + 5
+    i = i + 1
+}
+
+if (i < 10) {
+    b = b + 5
+    i = i + 1
+}
+if (i < 10) {
+    b = b + 5
+    i = i + 1
+}
+
+
+console.log(b)
