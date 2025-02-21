6a6ece8222ffb1f3420700aa8fd8052345533678
DianaGC <garcapdia@hotmail.com>
2025-02-20 20:49:52 -0600

    script clase6

--- Clase6/Manejo_inputs/script.js
+++ Clase6/Manejo_inputs/script.js
@@ -8,6 +8,10 @@ button.addEventListener("click", () => {
 
     console.log("El nombre del usuario es: "  + nombre.value)
 
+    // Para poner HTML debajo y de forma multiple
+    // original_html = result.innerHTML
+    // result.innerHTML += "<p> "Hola + nombre.value + " <p><br>"
+
     result.innerHTML = "Bienvenido " + nombre.value
 })
 
